2089681a3a54c3cdcd32b41c62888949a802fbb9
xukangqi <331705233@qq.com>
2018-10-18 14:14:37 +0800

    添加公有结果类

--- src/main/java/com/bank/controller/TestController.java
+++ src/main/java/com/bank/controller/TestController.java
@@ -2,6 +2,7 @@ package com.bank.controller;
 
 import com.bank.pojo.BankUser;
 import com.bank.service.TestService;
+import com.bank.utils.BankResult;
 import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -29,54 +30,52 @@ public class TestController {
     //请求格式 /user/hello/1
     @RequestMapping(value = "/hello/{id}",method = RequestMethod.GET)
     @ResponseBody
-    public JSONObject hello(@PathVariable("id")int id){
+    public BankResult hello(@PathVariable("id")int id){
         JSONObject jsonObject=new JSONObject();
         //service层操作
 
 
         //返回结果
-        jsonObject.put("result","success");
-        return  jsonObject;
+        return  BankResult.ok();
     }
 
     //请求格式 /user/hello2/id=123&password=123
     @RequestMapping(value = "/hello2",method = RequestMethod.GET)
     @ResponseBody
-    public JSONObject hello2(@RequestParam(value = "id")int id,@RequestParam(value = "password")String password){
+    public BankResult hello2(@RequestParam(value = "id")int id,@RequestParam(value = "password")String password){
         JSONObject jsonObject=new JSONObject();
         //service层操作
 
 
         //返回结果
-        jsonObject.put("result","success");
-        return  jsonObject;
+        BankResult bankResult=new BankResult();
+        bankResult.setData(new BankUser());
+        bankResult.setMsg("error");
+        bankResult.setStatus(501);
+        return  bankResult;
     }
 
     //post请求格式 /user/hello3
     @RequestMapping(value = "/hello3",method = RequestMethod.POST)
     @ResponseBody
-    public JSONObject hello3(@RequestParam(value = "id")int id,@RequestParam(value = "password")String password){
+    public BankResult hello3(@RequestParam(value = "id")int id,@RequestParam(value = "password")String password){
         JSONObject jsonObject=new JSONObject();
         //service层操作
 
 
         //返回结果
-        jsonObject.put("result","success");
-        return  jsonObject;
+        return  BankResult.ok();
     }
 
     //post请求 /user/hello4 前端发送一个User类，后端自动注入
     @RequestMapping(value = "/hello4",method = RequestMethod.POST)
     @ResponseBody
-    public JSONObject hello4(BankUser user){
+    public BankResult hello4(BankUser user){
         JSONObject jsonObject=new JSONObject();
         //service层操作
 
 
-        //返回结果
-        jsonObject.put("result","success");
-        jsonObject.put("message","123");
-        jsonObject.put("data",new BankUser());
-        return  jsonObject;
+       //返回结果
+        return  BankResult.ok();
     }
 }
